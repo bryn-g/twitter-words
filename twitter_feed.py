3ba3ddc64275dac15949c80482cccfcbe90bd8b4
mishoryu <22555149+mishoryu@users.noreply.github.com>
2018-01-20 05:51:50 +1100

    Removed async flag on stream to handle timeouts.

--- twitter_feed.py
+++ twitter_feed.py
@@ -6,21 +6,25 @@ import json
 import html
 import textwrap
 import tweepy
+import ssl
+import time
 
 import twitter_helper
 import text_colorizer
 
 class FeedListener(tweepy.streaming.StreamListener):
-    #stop_count = 10
-    #tweet_count = 0
+    # tweet padding and line widths in chars
+    PADDING_WIDTH = 21
+    LINE_WIDTH = 90
 
+    # terminal color assignment
     term = text_colorizer.TermTextColorizer()
     term.add_iro("orange", "172")
-    term.add_iro("lorange", "220")
-    term.add_iro("lgreen", "112")
-    term.add_iro("lviolet", "140")
-    term.add_iro("dull", "246")
-    term.add_iro("bland", "96")
+    term.add_iro("gold", "220")
+    term.add_iro("green", "112")
+    term.add_iro("purple", "140")
+    term.add_iro("gray", "246")
+    term.add_iro("plum", "96")
 
     @classmethod
     def handle_urls(cls, urls, text, count, tag="URL"):
@@ -29,7 +33,7 @@ class FeedListener(tweepy.streaming.StreamListener):
             twitter_url = url['url']
             expanded_url = url['expanded_url']
             url_tag = f"[{tag}#{count}]"
-            color_tag = f"{cls.term.iro(url_tag, 'dull')}"
+            color_tag = f"{cls.term.iro(url_tag, 'gray')}"
             text = text.replace(twitter_url, color_tag)
             url_list.append(f"[{count}]{expanded_url}")
             count += 1
@@ -37,14 +41,9 @@ class FeedListener(tweepy.streaming.StreamListener):
         return url_list, text, count
 
     def on_status(self, status):
-        #if self.tweet_count == 1:
-        #    sys.exit()
-
         if hasattr(status, 'retweeted_status'):
             if status.retweeted_status:
                 return
-
-        #self.tweet_count += 1
 
         text = ""
         if hasattr(status, 'extended_tweet') and status.truncated:
@@ -90,13 +89,11 @@ class FeedListener(tweepy.streaming.StreamListener):
 
         text = html.unescape(text)
         text = re.sub('[ ]{2,}', ' ', text)
-        nl = self.term.iro(' • ', 'dull')
+        nl = self.term.iro(' • ', 'gray')
         text = re.sub('[\n]{1,}', nl, text)
 
-        #r'(@[A-Za-z0-9_]{1,15})\W'
-        text = re.sub(twitter_helper.TwitterHelper.TWEET_SCREEN_NAME_PATTERN, self.term.iro(r'\1', 'lorange'), text)
-        #text = twitter_helper.TwitterHelper.TWEET_SCREEN_NAME_PATTERN.sub(text)
-
+        # color twitter names in tweet
+        text = re.sub(twitter_helper.TwitterHelper.TWEET_SCREEN_NAME_PATTERN, self.term.iro(r'\1', 'gold'), text)
 
         quote_status = ""
         if hasattr(status, 'is_quote_status'):
@@ -108,33 +105,27 @@ class FeedListener(tweepy.streaming.StreamListener):
             if 'entities' in status.quoted_status:
                 if 'user_mentions' in status.quoted_status['entities']:
                     for user in status.quoted_status['entities']['user_mentions']:
-                        #quote_mentions += f" @{user['screen_name']}"
-                        quote_mentions += f" {self.term.iro('@' + user['screen_name'], 'bland')}"
+                        quote_mentions += f" {self.term.iro('@' + user['screen_name'], 'plum')}"
 
-        header_string = f"[{self.term.iro(status.created_at, 'lgreen')}]" + \
-                        f"[{self.term.iro('@' + status.user.screen_name, 'lviolet')}] {quote_status}{quote_mentions}"
+        header_string = f"[{self.term.iro(status.created_at, 'green')}]" + \
+                        f"[{self.term.iro('@' + status.user.screen_name, 'purple')}] {quote_status}{quote_mentions}"
 
         tweet_string = f"{text}"
         tweet_string = f"{textwrap.dedent(tweet_string).strip()}"
 
-        padding_length = 21
-        padding = ' '*padding_length
-        width = padding_length + 90
-        print(f"{textwrap.fill(header_string, width=width+padding_length, subsequent_indent=padding)}")
+        padding = ' '*self.PADDING_WIDTH
+        width = self.PADDING_WIDTH + self.LINE_WIDTH
+        print(f"{textwrap.fill(header_string, width=width+self.PADDING_WIDTH, subsequent_indent=padding)}")
         print(f"{textwrap.fill(tweet_string, width=width, initial_indent=padding, subsequent_indent=padding)}")
 
         if tweet_url_list:
-            urls = twitter_helper.insert_newlines(tweet_url_list, 90, padding_count=21, padding_first_line=True,
-                                                  newline_to_spaces=False)
+            urls = twitter_helper.insert_newlines(tweet_url_list, self.LINE_WIDTH, padding_count=self.PADDING_WIDTH,
+                                                  padding_first_line=True, newline_to_spaces=False)
 
-            color_urls = f"{self.term.iro(urls, 'dull')}"
+            color_urls = f"{self.term.iro(urls, 'gray')}"
             print(f"{color_urls}")
 
         print()
-
-        #twitter_helper.print_json(status._json)
-        # if self.tweet_count == self.stop_count:
-        #     exit()
 
     def on_error(self, status):
         if status == 420:
@@ -153,7 +144,7 @@ def get_arguments():
 
 def main():
     term = text_colorizer.TermTextColorizer()
-    term.add_iro("lblue", "38")
+    term.add_iro("blue", "38")
 
     twitter_api_keys = twitter_helper.get_twitter_env_api_keys()
     tweepy_auth = twitter_helper.get_tweepy_auth_handler(twitter_api_keys)
@@ -162,24 +153,18 @@ def main():
     keyword_list = user_args.keyword.split(',')
 
     twitter_stream = tweepy.Stream(tweepy_auth, FeedListener())
-    twitter_stream.filter(track=keyword_list, async=True)
 
-    key_press = ''
-    while key_press != 'q':
-        try:
-            key_press = input(f"{term.iro('Twitter feed is running. (enter q or ctrl-c to quit)', 'lblue')}\n")
-        except SyntaxError:
-            pass
-        except:
-            print(f"\n{term.iro('Stopping twitter feed.', 'lblue')}")
-            twitter_stream.disconnect()
-            sys.exit()
-
-    print(f"{term.iro('Stopping twitter feed.', 'lblue')}")
     try:
+        twitter_stream.filter(track=keyword_list, async=False)
+    except ssl.SSLError:
+        print(f"\n{term.iro('Connection timeout. Stopping twitter feed.', 'blue')}")
         twitter_stream.disconnect()
     except:
-        print(f"{term.iro('Stream disconnect error.', 'lblue')}")
+        print(f"\n{term.iro('Error. Stopping twitter feed.', 'blue')}")
+        try:
+            twitter_stream.disconnect()
+        except:
+            print(f"{term.iro('Stream disconnect error.', 'blue')}")
 
 if __name__ == '__main__':
     main()
